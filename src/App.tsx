87e29db3eb23a8402c7617c4d5cdc215e3766a69
dima <tynindima@gmail.com>
2020-06-29 09:57:33 +0300

    Create maze and arrows

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,41 @@ import React from 'react';
 import './App.scss';
 
 const App = () => {
+  const maze = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+  ];
+
+  const directions = [];
+
+  for (let i = 0; i < 10; i++) {
+    directions.push(i);
+  };
+
+  const handleSelect = (event: React.MouseEvent<HTMLTableDataCellElement>) => {
+    console.log(event.target);
+  };
+
   return (
     <div className="App">
-      Start
+      <table className="table">
+        <tbody>
+        {maze.map((_, i) => (
+          <tr key={i} className="table__row">
+            {maze[i].map((item, i) => (
+            <td onClick={handleSelect} className="table__digit" key={i}>{item}</td>
+            ))}
+          </tr>
+        ))}
+        </tbody>
+      </table>
+
+      <div className="directions">
+        {directions.map(item => (
+          <div className="directions__step" key={item}>{item}</div>
+        ))}
+      </div>
     </div>
   );
 };
