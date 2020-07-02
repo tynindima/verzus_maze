4d998bdfbcb5e330a72c864031755ce6afc4a887
dima <tynindima@gmail.com>
2020-07-02 11:28:44 +0300

    Updates

---
+++ precache-manifest.58c677d3b3e61b58fc9f4088f1a4368e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60a539392e976355ecda09c8b81fbdac",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3ba6572c0d7a4fc363d6",
+    "url": "./static/css/main.07f772f3.chunk.css"
+  },
+  {
+    "revision": "ec5fefa9ba07ee1350cc",
+    "url": "./static/js/2.4c1630be.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.4c1630be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ba6572c0d7a4fc363d6",
+    "url": "./static/js/main.aa698836.chunk.js"
+  },
+  {
+    "revision": "5500bd99d58138eb21a7",
+    "url": "./static/js/runtime-main.78a77a75.js"
+  },
+  {
+    "revision": "2f5474a949f98209105c962ab5d6a002",
+    "url": "./static/media/arrow.2f5474a9.png"
+  },
+  {
+    "revision": "5b856668bc03e27048ffce9de24c4e92",
+    "url": "./static/media/start2.5b856668.png"
+  }
+]);
